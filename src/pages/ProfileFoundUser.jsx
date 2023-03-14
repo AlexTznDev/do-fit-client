4a335ad053fc0674085c1290f869bd257377ec07
Victor Vikhal <alfonso.vikhal@gmail.com>
2023-03-14 23:35:28 +0100

    solucionando problemas de renderizacion de informacion del usuario buscado

--- src/pages/ProfileFoundUser.jsx
+++ src/pages/ProfileFoundUser.jsx
@@ -1,97 +1,86 @@
-
 import AllButtons from "../components/AllButtons";
 import { Link, useNavigate } from "react-router-dom";
 import { useEffect, useState } from "react";
-import {  followFoundUserService, profileSerivce, profileUserService } from "../services/profile.services";
+import {
+  followFoundUserService,
+  profileSerivce,
+  profileUserService,
+} from "../services/profile.services";
 import { useContext } from "react";
 import { AuthContext } from "../context/auth.context";
 import { useParams } from "react-router-dom";
 import { routineUserService } from "../services/routine.services";
 
-
 function ProfileFoundUser() {
+  const params = useParams();
+  const navigate = useNavigate();
+
+  const { idFoundUser } = params;
   
-    const params = useParams()
-    const navigate = useNavigate();
 
-    const {idFoundUser} = params
-    console.log(idFoundUser)
+  //const { isLoggedIn, authenticateUser } = useContext(AuthContext);
 
-    //const { isLoggedIn, authenticateUser } = useContext(AuthContext);
-  
-    const [userData, setUserData] = useState(null);
-    const [isFetching, setisFetching] = useState(true);
+  const [userData, setUserData] = useState(null);
+  const [isFetching, setisFetching] = useState(true);
 
-    const [allRoutines, setallRoutines] = useState(null);
-    const [isFetchingRoutine, setisFetchingRoutine] = useState(true);
+  const [allRoutines, setallRoutines] = useState(null);
+  const [isFetchingRoutine, setisFetchingRoutine] = useState(true);
 
-    const [ visitorData, setVisitorData ] = useState(null)
-    const [ areFriends, setAreFriends ] = useState(false)
-  
-  
-    useEffect(() => {
-      getData();
-      getDataAllRoutines();
+  const [visitorData, setVisitorData] = useState(null);
+  const [areFriends, setAreFriends] = useState(false);
 
-    }, []);
+  useEffect(() => {
+    getData();
+    getDataAllRoutines();
+  }, []);
 
+  const getDataAllRoutines = async () => {
+    try {
+      const response = await routineUserService(idFoundUser);
 
-  
-    const getDataAllRoutines = async () => {
-      try {
-        const response = await routineUserService(idFoundUser)
-        
-        setallRoutines(response.data);
-        console.log(response)
-        setisFetchingRoutine(false);
-      } catch (error) {
-        console.log(error);
-      }
-    };
-  
-    const getData = async () => {
-      setisFetching(true);
-  
-      try {
-        const response = await profileUserService(idFoundUser) 
-        console.log(response.data);
-    
-        const response2 = await profileSerivce()
-        console.log(response2.data)
-
-        setUserData(response.data);
-        setVisitorData(response2.data)
-        setisFetching(false);
-
-      } catch (error) {
-        console.log(error);
-      }
-    };
-
-    const followFoundUser = async() => {
-
-      try {
-        const response = await followFoundUserService(idFoundUser)
-        console.log(response)
-        setAreFriends(true)
-        
-        
-        
-
-      } catch (error) {
-        console.log(error)
-      }
-
+      setallRoutines(response.data);
+      console.log(response);
+      setisFetchingRoutine(false);
+    } catch (error) {
+      console.log(error);
     }
-  
-    if (isFetching === true) {
-      return;
-      <h1>...Searching</h1>;
+  };
+
+  const getData = async () => {
+    setisFetching(true);
+
+    try {
+      const response = await profileUserService(idFoundUser);
+      //console.log(response.data);
+
+      const response2 = await profileSerivce();
+     // console.log(response2.data);
+
+      setUserData(response.data);
+      setVisitorData(response2.data);
+      setisFetching(false);
+    } catch (error) {
+      console.log(error);
     }
-  
+  };
+
+  const followFoundUser = async () => {
+    try {
+      const response = await followFoundUserService(idFoundUser);
+      //console.log(response);
+      setAreFriends(true);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  if (isFetching === true) {
+    return;
+    <h1>...Searching</h1>;
+  }
+
   return (
     <div className="mainContainer">
-      
       <div style={{ borderStyle: "solid", width: "30vw" }}>
         <img src={userData.imageProfile} alt="img" width="200px" />
         <div>
@@ -108,19 +97,20 @@ function ProfileFoundUser() {
 
       <br />
       <br />
-      <AllButtons/>
-      {visitorData.friends.includes(idFoundUser) ? null : (areFriends ? null : <button type="submit" onClick={followFoundUser}>Follow</button>)}
-      
-      
-      
+      <AllButtons />
+      {visitorData.friends.includes(idFoundUser) ? null : areFriends ? null : (
+        <button type="submit" onClick={followFoundUser}>
+          Follow
+        </button>
+      )}
 
       <br />
       <br />
 
-      {isFetchingRoutine ? 
+      {isFetchingRoutine ? (
         <h2>...is fetching</h2>
-       : 
-        allRoutines.map((eachRoutine) => {
+      ) : (
+        allRoutines.response1.map((eachRoutine) => {
           return (
             <Link key={eachRoutine._id} to={`/routine/${eachRoutine._id}/user`}>
               <div>
@@ -129,11 +119,9 @@ function ProfileFoundUser() {
             </Link>
           );
         })
-      }
-
-      
+      )}
     </div>
-  )
+  );
 }
 
-export default ProfileFoundUser
+export default ProfileFoundUser;
